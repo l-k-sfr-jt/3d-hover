4f0b3e8c3d9a5b598b488bab1a5c6f9e3570712e
l-k-sfr-jt <lukas3197@gmail.com>
2023-03-08 23:45:31 +0100

    css only option

--- main.js
+++ main.js
@@ -8,11 +8,14 @@ const rotateElement = function (event, element) {
     const x = event.clientX;
     const y = event.clientY;
 
-    const middleX = window.innerHeight / 2;
-    const middleY = window.innerWidth / 2;
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight / 2;
 
-    const offsetX = ((x - middleX) / middleX);
-    const offsetY = ((y - middleY) / middleY);
+    const offsetX = ((x - middleX) / middleX) * 45;
+    const offsetY = ((y - middleY) / middleY) * 45;
+
+    element.style.setProperty('--rotateX', -offsetY + 'deg');
+    element.style.setProperty('--rotateY', offsetX + 'deg')
 
 }
 
